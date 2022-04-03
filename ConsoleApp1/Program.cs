db34be6e06cb6e944854e6b66c9755d8b9d4bf80
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-03 19:46:18 +0400

    added new C# document

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 7;
+            int c = 9;
+            int d = 25;
+            int result = 0;
+            result = (a + b + c + d) / 5;
+            Console.WriteLine(result);
+        }
+    }
+}
